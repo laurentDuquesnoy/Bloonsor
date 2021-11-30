38275d8a2d4d5a58d0a1e0ac7b561d47d742c6ad
Developer0108 <laurentduquesnoy5@gmail.com>
2021-11-30 16:32:41 +0100

    added project files, basic functionality

---
+++ Bloonsor/Model/Balloon.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloonsor
+{
+    public class Balloon
+    {
+        public int PositionLeft { get; set; }
+        public int PositionTop { get; set; }
+        public int BalloonIndex { get; set; }
+    }
+}
